0ac22aa84f9898d42319797f5142d5628d0d0243
Bisminee <fiksilmiah@gmail.com>
2025-02-28 18:11:38 +0700

    Tugas 1

---
+++ jobsheet3/Dosen07.java
@@ -0,0 +1,13 @@
+public class Dosen07 {
+    public String kode;
+    public String nama;
+    public boolean gender;
+    public int usia;
+
+    public Dosen07(String kode, String nama, boolean gender, int usia){
+        this.kode = kode;
+        this.nama = nama;
+        this.gender = gender;
+        this.usia = usia;
+    }
+}
---
+++ jobsheet3/DosenDemo07.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+
+public class DosenDemo07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Dosen07 arrOfDosen[] = new Dosen07[3];
+        String nama, kode, jenisKelamin;
+        boolean gender;
+        int usia;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\nMasukkan Data Dosen ke-" + (i+1));
+            System.out.print("Masukkan kode Dosen: ");
+            kode = sc.nextLine();
+            System.out.print("Masukkan nama Dosen: ");
+            nama = sc.nextLine();
+            System.out.print("Masukkan jenis kelamin Dosen(pria/wanita): ");
+            jenisKelamin = sc.nextLine();
+            if(jenisKelamin.equalsIgnoreCase("pria")){
+                gender = true;
+            }else if(jenisKelamin.equalsIgnoreCase("wanita")){
+                gender = false;
+            }else{
+                gender = false;
+            }
+            System.out.print("Masukkan usia Dosen: ");
+            usia = sc.nextInt();
+            sc.nextLine();
+            arrOfDosen[i] = new Dosen07(kode, nama, gender, usia);
+            System.out.println("---------------------------------------");
+        }
+
+        for (Dosen07 obj : arrOfDosen) {
+            System.out.println("Kode Dosen: " + obj.kode );
+            System.out.println("Nama Dosen: " + obj.nama );
+            System.out.println("Usia Dosen: " + obj.usia );
+            if(obj.gender == true){
+                System.out.println("Jenis Kelamin Dosen: Pria"  );
+            }else{
+                System.out.println("Jenis Kelamin Dosen: Wanita"  );
+            }
+            System.out.println("---------------------------------------");
+        }
+
+    }
+}
