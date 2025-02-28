d980fcab20bdad94e095b183cf7a355152360d17
Bisminee <fiksilmiah@gmail.com>
2025-02-28 14:09:09 +0700

    Jobsheet3

---
+++ jobsheet3/MataKuliahDemo07.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class MataKuliahDemo07 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MataKuliah07 arrayOfMataKulaih[] = new MataKuliah07[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data Mata Kulaih ke-" + (i + 1));
+            System.out.print("Kode          : ");
+            kode = sc.nextLine();
+            System.out.print("Nama          : ");
+            nama = sc.nextLine();
+            System.out.print("SKS           : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam    : ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("---------------------------------");
+
+            arrayOfMataKulaih[i] = new MataKuliah07(kode, nama, sks, jumlahJam);
+        }
+    }
+}
