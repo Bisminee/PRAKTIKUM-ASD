45bb61b5efd5f86d04f6cda156430364995348ca
Bisminee <fiksilmiah@gmail.com>
2025-03-19 20:43:57 +0700

    pertanyaan

--- jobsheet5/Pangkat.java
+++ jobsheet5/Pangkat.java
@@ -1,29 +1,31 @@
+
 class Pangkat {
+
     int nilai, pangkat;
 
-    public Pangkat(int n,int p) {
+    public Pangkat(int n, int p) {
         nilai = n;
         pangkat = p;
     }
 
-    int pangkatBF(int a, int n){
+    int pangkatBF() {
         int hasil = 1;
-        for(int i = 0; i < n; i++){
-            hasil = hasil * a;
+        for (int i = 0; i < pangkat; i++) {
+            hasil = hasil * nilai;
         }
         return hasil;
     }
 
-    int pangkatDC(int a, int n){
-        if(n==1){
+    int pangkatDC(int a, int n) {
+        if (n == 1) {
             return a;
-        }else{
-            if(n%2==1){
-                return (pangkatDC(a, n/2) * pangkatDC(a, n/2)*a);
-            }else{
-                return (pangkatDC(a, n/2) * pangkatDC(a, n/2));
+        } else {
+            if (n % 2 == 1) {
+                return (pangkatDC(a, n / 2) * pangkatDC(a, n / 2) * a);
+            } else {
+                return (pangkatDC(a, n / 2) * pangkatDC(a, n / 2));
             }
         }
     }
-    
+
 }
