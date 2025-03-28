2440f6e1474ed0e603f90efd4b623c23ee0729cb
Bisminee <fiksilmiah@gmail.com>
2025-03-28 10:51:14 +0700

    Praktikum 2

---
+++ Jobsheet6/Mahasiswa07.java
@@ -0,0 +1,23 @@
+
+public class Mahasiswa07 {
+
+    String nim, nama, kelas;
+    double ipk;
+
+    Mahasiswa07() {
+    }
+
+    public Mahasiswa07(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        kelas = kls;
+        ipk = ip;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+}
